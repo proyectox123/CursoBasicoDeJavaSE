7f75285e1bb9f70e3f8cee69ec657fb1dfe46c9a
MHO <mauricio.hdez.o.10@gmail.com>
2018-02-17 23:56:12 -0600

    First example.

--- HelloWorldExample/src/HelloWorld.java
+++ HelloWorldExample/src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.println("Hello World! (Again :V)");
+        System.out.println("Hello world!!!!");
     }
 
 }
