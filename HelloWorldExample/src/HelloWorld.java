b3417b0664f94cb9e532f4d5bf07dcf17848ee74
MHO <mauricio.hdez.o.10@gmail.com>
2018-03-07 22:27:55 -0600

    Tipos de datos primitivos enteros.

--- HelloWorldExample/src/HelloWorld.java
+++ HelloWorldExample/src/HelloWorld.java
@@ -2,6 +2,16 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello world!!!!");
+
+        primitiveData();
+    }
+
+    private static void primitiveData(){
+        //Enteros
+        byte edad = 127;
+        short year = 2020;
+        int idUser = 1001;
+        long idTwitter = 12345678923456789L;
     }
 
 }
