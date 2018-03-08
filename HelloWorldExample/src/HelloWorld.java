282a262c70479e0b2e3e78b04539dcb44583104b
MHO <mauricio.hdez.o.10@gmail.com>
2018-03-07 22:37:06 -0600

    Tipos de datos con punto decimal.

--- HelloWorldExample/src/HelloWorld.java
+++ HelloWorldExample/src/HelloWorld.java
@@ -9,9 +9,19 @@ public class HelloWorld {
     private static void primitiveData(){
         //Enteros
         byte edad = 127;
-        short year = 2020;
-        int idUser = 1001;
-        long idTwitter = 12345678923456789L;
+        short year = 2020;//2 bytes
+        int idUser = 1001;//4 bytes
+        long idTwitter = 12345678923456789L;//8 bytes
+
+        //Punto Flotante
+        float diameter = 35.25F;//4 bytes
+        double price = 12345.23456789234;//8 bytes
+
+        //Textos
+        char gender = 'M';//2 bytes
+
+        //Lògicos
+        boolean isVisible = false;//2 bytes
     }
 
 }
